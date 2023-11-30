ef025db653935925073af4f656611a62d5f41e34
saikat112 <saikatmanna112@gmail.com>
2023-11-30 14:46:03 +0530

    dubg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import NavBar from ".components/NavBar";
+import Body from ".components/Body";
+import Footer from ".components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        <Body/>
+        <Footer/>
       </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,8 @@
+import React from "react";
+function Home() {
+    return
+    (
+        <p>hello</p>
+    );
+}
+export default Home;
